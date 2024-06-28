559a04c35081abce9c0608bb8d0c461767c6d548
Agonist <bastien.billey@gmail.com>
2024-06-28 09:27:16 +0800

    add key

--- src/app/_components/loading.tsx
+++ src/app/_components/loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="flex flex-col space-y-4">
       {[1, 2, 3, 4, 5].map((i) => (
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2" key={i}>
           <Skeleton className="h-4 w-96" />
           <Skeleton className="h-3 w-96" />
         </div>
